09eefc1b87bc01b0bbe7fa64def310926ff4096d
Lisa DN <dorvillelisa17@gmail.com>
2022-11-08 14:38:08 +0100

    installation multer

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -34,19 +34,17 @@ exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
-        return res
-          .staus(401)
-          .json({ message: "Login ou mot de passe incorrecte" });
+        res.status(401).json({ message: "Login ou mot de passe incorrecte" });
       }
       bcrypt
         .compare(req.body.password, user.password)
         .then((valid) => {
           if (!valid) {
-            return res
+            res
               .status(401)
               .json({ message: "Login ou mot de passe incorrecte" });
           }
-          res.staus(200).json({
+          res.status(200).json({
             userId: user._id,
             token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
               expiresIn: "24h",
